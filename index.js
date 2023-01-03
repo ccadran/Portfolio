8422424466b6f167308c9f76048b565df2e7f162
ccadran <clariocadran33@gmail.com>
2023-01-03 12:32:05 +0100

    anim

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const skillsContainers = document.querySelectorAll(".skills-container");
 function skillsHover() {
     Array.prototype.forEach.call(skillsContainers, (container) => {
         container.addEventListener("mouseenter", () => {
+            console.log("testtt");
             const logoSkills = container.querySelectorAll(".logo-skills");
             Array.prototype.forEach.call(logoSkills, (logo) => {
                 logo.style.display = "flex";
@@ -21,16 +22,36 @@ function skillsHover() {
     });
 }
 skillsHover();
+
 let lastScroll = 0;
 const nav = document.querySelector("nav");
 console.log(nav);
 window.addEventListener("scroll", () => {
     if (window.scrollY < lastScroll) {
-        console.log("test1");
         nav.style.top = 0;
     } else {
         nav.style.top = "-100px";
     }
 
     lastScroll = window.scrollY;
+});
+
+const project = document.querySelectorAll(".project ");
+Array.prototype.forEach.call(project, (project) => {
+    console.log(project);
+
+    project.addEventListener("mouseenter", () => {
+        project.style.scale = "1.1";
+        const imgs = project.querySelectorAll("img");
+        Array.prototype.forEach.call(imgs, (img) => {
+            img.style.filter = "grayscale(0%)";
+        });
+    });
+    project.addEventListener("mouseleave", () => {
+        project.style.scale = "1";
+        const imgs = project.querySelectorAll("img");
+        Array.prototype.forEach.call(imgs, (img) => {
+            img.style.filter = "grayscale(100%)";
+        });
+    });
 });
