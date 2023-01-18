84c4bb4ae9afee3b6131c2e1b6e0c07dd1714c7e
ccadran <clariocadran33@gmail.com>
2023-01-18 22:29:02 +0100

    responsive done

--- Project/project.js
+++ Project/project.js
@@ -2,12 +2,54 @@ let lastScroll = 0;
 const nav = document.querySelector("nav");
 console.log(nav);
 window.addEventListener("scroll", () => {
-    if (window.scrollY < lastScroll) {
-        console.log("test1");
-        nav.style.top = 0;
+    if (btnBurger.classList.contains("active")) {
     } else {
-        nav.style.top = "-100px";
+        if (window.scrollY < lastScroll) {
+            nav.style.top = 0;
+        } else {
+            nav.style.top = "-100px";
+        }
     }
 
     lastScroll = window.scrollY;
 });
+
+const btnBurger = document.getElementById("btn");
+const navUl = document.querySelector(".nav-container ul");
+const body = document.querySelector("body");
+
+btnBurger.addEventListener("click", () => {
+    btnBurger.classList.toggle("active");
+    navUl.classList.toggle("active");
+    nav.classList.toggle("active");
+    if (btnBurger.classList.contains("active")) {
+        console.log(window.scrollY);
+        navUl.style.height = window.innerHeight + "px";
+        navUl.style.width = window.innerWidth + "px";
+        // navUl.style.top = window.scrollY - 10 + "px";
+        // console.log(window.scrollY);
+        body.style = "overflow:hidden";
+    } else {
+        body.style = "overflow:scroll";
+    }
+});
+
+function btnBurgerOnScroll() {
+    let lastScroll2 = 0;
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 60) {
+            if (window.scrollY < lastScroll2) {
+                console.log(window.scrollY);
+                btnBurger.style.top = "30px";
+                console.log(lastScroll2);
+            } else {
+                btnBurger.style.top = "-60px";
+            }
+
+            lastScroll2 = window.scrollY;
+        }
+    });
+}
+if (innerWidth < 1023) {
+    btnBurgerOnScroll();
+}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const skillsContainers = document.querySelectorAll(".skills-container");
-
+const btnBurger = document.getElementById("btn");
+const navUl = document.querySelector(".nav-container ul");
+const body = document.querySelector("body");
 function skillsHover() {
     Array.prototype.forEach.call(skillsContainers, (container) => {
         container.addEventListener("mouseenter", () => {
@@ -27,10 +29,13 @@ let lastScroll = 0;
 const nav = document.querySelector("nav");
 console.log(nav);
 window.addEventListener("scroll", () => {
-    if (window.scrollY < lastScroll) {
-        nav.style.top = 0;
+    if (btnBurger.classList.contains("active")) {
     } else {
-        nav.style.top = "-100px";
+        if (window.scrollY < lastScroll) {
+            nav.style.top = 0;
+        } else {
+            nav.style.top = "-100px";
+        }
     }
 
     lastScroll = window.scrollY;
@@ -55,3 +60,39 @@ Array.prototype.forEach.call(project, (project) => {
         });
     });
 });
+
+btnBurger.addEventListener("click", () => {
+    btnBurger.classList.toggle("active");
+    navUl.classList.toggle("active");
+    nav.classList.toggle("active");
+    if (btnBurger.classList.contains("active")) {
+        console.log(window.scrollY);
+        navUl.style.height = window.innerHeight + "px";
+        navUl.style.width = window.innerWidth + "px";
+        // navUl.style.top = window.scrollY - 10 + "px";
+        // console.log(window.scrollY);
+        body.style = "overflow:hidden";
+    } else {
+        body.style = "overflow:scroll";
+    }
+});
+
+function btnBurgerOnScroll() {
+    let lastScroll2 = 0;
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 60) {
+            if (window.scrollY < lastScroll2) {
+                console.log(window.scrollY);
+                btnBurger.style.top = "30px";
+                console.log(lastScroll2);
+            } else {
+                btnBurger.style.top = "-60px";
+            }
+
+            lastScroll2 = window.scrollY;
+        }
+    });
+}
+if (innerWidth < 1023) {
+    btnBurgerOnScroll();
+}
