d4575f94f10a4c9aee99c6df78e44952b8489745
ccadran <clariocadran33@gmail.com>
2023-04-27 17:15:06 +0200

    add film tracker project + hover effect modification

--- index.js
+++ index.js
@@ -6,70 +6,62 @@ const mouses = document.querySelectorAll(".mouse");
 const burgerChilds = document.querySelectorAll(".burger-child");
 
 function skillsHover() {
-    Array.prototype.forEach.call(skillsContainers, (container) => {
-        container.addEventListener("mouseenter", () => {
-            const logoSkills = container.querySelectorAll(".logo-skills");
-            Array.prototype.forEach.call(logoSkills, (logo) => {
-                logo.style.display = "flex";
-            });
-            const title = container.querySelector("p");
-            title.style.top = "30px";
-        });
-        container.addEventListener("mouseleave", () => {
-            const logoSkills = container.querySelectorAll(".logo-skills");
-            Array.prototype.forEach.call(logoSkills, (logo) => {
-                logo.style.display = "none";
-            });
-            const title = container.querySelector("p");
-            title.style.top = "";
-        });
+  Array.prototype.forEach.call(skillsContainers, (container) => {
+    container.addEventListener("mouseenter", () => {
+      const logoSkills = container.querySelectorAll(".logo-skills");
+      Array.prototype.forEach.call(logoSkills, (logo) => {
+        logo.style.display = "flex";
+      });
+      const title = container.querySelector("p");
+      title.style.top = "30px";
     });
+    container.addEventListener("mouseleave", () => {
+      const logoSkills = container.querySelectorAll(".logo-skills");
+      Array.prototype.forEach.call(logoSkills, (logo) => {
+        logo.style.display = "none";
+      });
+      const title = container.querySelector("p");
+      title.style.top = "";
+    });
+  });
 }
 skillsHover();
 
 let lastScroll = 0;
 const nav = document.querySelector("nav");
 window.addEventListener("scroll", () => {
-    if (btnBurger.classList.contains("active")) {
+  if (btnBurger.classList.contains("active")) {
+  } else {
+    if (window.scrollY < lastScroll) {
+      nav.style.top = 0;
     } else {
-        if (window.scrollY < lastScroll) {
-            nav.style.top = 0;
-        } else {
-            nav.style.top = "-100px";
-        }
+      nav.style.top = "-100px";
     }
+  }
 
-    lastScroll = window.scrollY;
+  lastScroll = window.scrollY;
 });
 function blackImg() {
-    if (window.matchMedia("(min-width: 968px)").matches) {
-        const project = document.querySelectorAll(".project ");
-        const imgs = document.querySelectorAll(".project img");
-        Array.prototype.forEach.call(imgs, (img) => {
-            img.style.filter = "grayscale(100%)";
-        });
-        Array.prototype.forEach.call(project, (project) => {
-            project.addEventListener("mouseenter", () => {
-                project.style.scale = "1.1";
-
-                Array.prototype.forEach.call(imgs, (img) => {
-                    img.style.filter = "grayscale(0%)";
-                });
-            });
-            project.addEventListener("mouseleave", () => {
-                project.style.scale = "1";
-                const imgs = project.querySelectorAll("img");
-                Array.prototype.forEach.call(imgs, (img) => {
-                    img.style.filter = "grayscale(100%)";
-                });
-            });
-        });
-    } else {
-        const imgs = document.querySelectorAll(".project img");
-        Array.prototype.forEach.call(imgs, (img) => {
-            img.style.filter = "grayscale(0%)";
-        });
-    }
+  if (window.matchMedia("(min-width: 968px)").matches) {
+    const projects = document.querySelectorAll(".project");
+    Array.prototype.forEach.call(projects, (project) => {
+      const img = project.querySelector("img");
+      img.style.filter = "grayscale(100%)";
+      project.addEventListener("mouseenter", () => {
+        project.style.scale = "1.1";
+        img.style.filter = "grayscale(0%)";
+      });
+      project.addEventListener("mouseleave", () => {
+        project.style.scale = "1";
+        img.style.filter = "grayscale(100%)";
+      });
+    });
+  } else {
+    const imgs = document.querySelectorAll(".project img");
+    Array.prototype.forEach.call(imgs, (img) => {
+      img.style.filter = "grayscale(0%)";
+    });
+  }
 }
 blackImg();
 window.addEventListener("resize", blackImg);
@@ -78,42 +70,42 @@ window.addEventListener("resize", blackImg);
 //     console.log("test img");
 // }
 btnBurger.addEventListener("click", () => {
-    btnBurger.classList.toggle("active");
-    navUl.classList.toggle("active");
-    nav.classList.toggle("active");
-    if (btnBurger.classList.contains("active")) {
-        navUl.style.height = window.innerHeight + "px";
-        navUl.style.width = window.innerWidth + "px";
+  btnBurger.classList.toggle("active");
+  navUl.classList.toggle("active");
+  nav.classList.toggle("active");
+  if (btnBurger.classList.contains("active")) {
+    navUl.style.height = window.innerHeight + "px";
+    navUl.style.width = window.innerWidth + "px";
 
-        body.style = "overflow:hidden";
-    } else {
-        body.style = "overflow:scroll";
-    }
+    body.style = "overflow:hidden";
+  } else {
+    body.style = "overflow:scroll";
+  }
 });
 burgerChilds.forEach((child) => {
-    if (
-        child.addEventListener("click", () => {
-            btnBurger.classList.remove("active");
-            navUl.classList.remove("active");
-            nav.classList.remove("active");
-        })
-    );
+  if (
+    child.addEventListener("click", () => {
+      btnBurger.classList.remove("active");
+      navUl.classList.remove("active");
+      nav.classList.remove("active");
+    })
+  );
 });
 
 function btnBurgerOnScroll() {
-    let lastScroll2 = 0;
-    window.addEventListener("scroll", () => {
-        if (window.scrollY > 60) {
-            if (window.scrollY < lastScroll2) {
-                btnBurger.style.top = "30px";
-            } else {
-                btnBurger.style.top = "-60px";
-            }
+  let lastScroll2 = 0;
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 60) {
+      if (window.scrollY < lastScroll2) {
+        btnBurger.style.top = "30px";
+      } else {
+        btnBurger.style.top = "-60px";
+      }
 
-            lastScroll2 = window.scrollY;
-        }
-    });
+      lastScroll2 = window.scrollY;
+    }
+  });
 }
 if (innerWidth < 1023) {
-    btnBurgerOnScroll();
+  btnBurgerOnScroll();
 }
